0fe338f848c896944ddd963f400dbceafeea97ff
tmnpierre <tomyn.pierre@gmail.com>
2023-12-19 19:09:42 +0100

    feat(Exercice01): update Exercice01

--- Exercice01/Program.cs
+++ Exercice01/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("*** Ma liste de todo ***");
+Console.WriteLine("Aujourd'hui je dois faire : ");
+Console.WriteLine("\t-Apprendre le C#");
+Console.WriteLine("\t-Apprendre à utiliser Visual Studio");
+Console.WriteLine("\t-Comprendre l'affichage \"Console\"");
+Console.WriteLine("\t-Créer mon répertoire \"c:\\MesExercices\\\" pour les ranger");
+Console.WriteLine("\t-Apprécier les fonctionnalités du C#");
+
+Console.WriteLine("*** Ma liste de todo ***\nAujourd'hui je dois faire : \n\t-Apprendre le C#\n\t-Apprendre à utiliser Visual Studio\n\t-Comprendre l'affichage \"Console\"\n\t-Créer mon répertoire \"c:\\MesExercices\\\" pour les ranger\n\t-Apprécier les fonctionnalités du C#");
