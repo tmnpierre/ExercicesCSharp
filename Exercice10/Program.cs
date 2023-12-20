53b8a6a06104b5cc04c1e113ba7902780a7ac0f5
tmnpierre <tomyn.pierre@gmail.com>
2023-12-20 15:48:26 +0100

    feat: do exercice

---
+++ Exercice10/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.Design;
+
+Console.Write("Entrer une lettre: ");
+var voyelle = Char.Parse(Console.ReadLine());
+
+if (voyelle == 'a' || voyelle == 'e' || voyelle == 'i' || voyelle == 'o' || voyelle == 'u' || voyelle == 'y')
+    Console.WriteLine("Cette lettre est une voyelle");
+
+else Console.WriteLine("Cette lettre n'est pas une voyelle");
