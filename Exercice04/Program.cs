02f8b4b02653c1f5ea0568b7bc78a7716584920b
tmnpierre <tomyn.pierre@gmail.com>
2023-12-20 10:33:09 +0100

    feat(Exercice05): do Exercice05

--- Exercice04/Program.cs
+++ Exercice04/Program.cs
@@ -3,5 +3,5 @@ var lastName = Console.ReadLine();
 Console.WriteLine("Veuillez saisir votre prénom :");
 var name = Console.ReadLine();
 Console.WriteLine("Veuilez saisir votre âge :");
-var age = Console.ReadLine();
+var age = int.Parse(Console.ReadLine());
 Console.WriteLine("Bonjour " + name + " " + lastName + ", vous avez " + age + " ans.");
---
+++ Exercice05/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Veuilez saisir un nombre :");
+var a = int.Parse(Console.ReadLine());
+Console.Write("Veuillez saisir un nombre :");
+var  b = int.Parse(Console.ReadLine());
+var sum = a + b;
+Console.WriteLine("La somme de ces deux nombre est : " + sum);
