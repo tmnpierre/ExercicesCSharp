04017717694cd0d87987ef4aa14a6e724073aa2b
tmnpierre <tomyn.pierre@gmail.com>
2023-12-20 10:15:51 +0100

    feat(Exercice03): do Exercice03

---
+++ Exercice03/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Veuillez saisir votre nom:");
+var lastName = Console.ReadLine();
+Console.WriteLine("Veuillez saisir votre prénom :");
+var name = Console.ReadLine();
+Console.WriteLine("Bonjour " + name + " " + lastName);
