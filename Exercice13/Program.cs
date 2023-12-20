2722832f0042c7910496dd80d7ff68935a0d89f8
tmnpierre <tomyn.pierre@gmail.com>
2023-12-20 20:36:32 +0100

    feat: do exercice

--- Exercice13/Program.cs
+++ Exercice13/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Entrez la longueur de AB en cm:");
+double ab = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Entrez la longueur de BC en cm:");
+double bc = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Entrez la longueur de CA en cm:");
+double ca = Convert.ToDouble(Console.ReadLine());
+
+if (ab == bc && bc == ca)
+{
+    Console.WriteLine("Le triangle est équilatéral.");
+}
+else if (ab == ca && ab != bc)
+{
+    Console.WriteLine("Le triangle est isocèle en A mais pas équilatéral.");
+}
+else if (ab == bc && ab != ca)
+{
+    Console.WriteLine("Le triangle est isocèle en B mais pas équilatéral.");
+}
+else if (ca == bc && ca != ab)
+{
+    Console.WriteLine("Le triangle est isocèle en C mais pas équilatéral.");
+}
+else
+{
+    Console.WriteLine("Le triangle n'est isocèle ni en A, ni en B, ni en C.");
+}
