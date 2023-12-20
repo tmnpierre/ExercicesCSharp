7a1e327082878ef224a244a08412d1cb39b809ce
tmnpierre <tomyn.pierre@gmail.com>
2023-12-20 10:21:36 +0100

    feat(Exercice04): do Exercice04

---
+++ Exercice04/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Veuillez saisir votre nom :");
+var lastName = Console.ReadLine();
+Console.WriteLine("Veuillez saisir votre prénom :");
+var name = Console.ReadLine();
+Console.WriteLine("Veuilez saisir votre âge :");
+var age = Console.ReadLine();
+Console.WriteLine("Bonjour " + name + " " + lastName + ", vous avez " + age + " ans.");
