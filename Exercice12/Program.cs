4b8cc7991c163656114fd6ea0c094c41a09a4b68
tmnpierre <tomyn.pierre@gmail.com>
2023-12-20 16:30:53 +0100

    feat: do exercice

---
+++ Exercice12/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Indiquez l'âge de votre enfant: ");
+
+if (int.TryParse(Console.ReadLine(), out int a))
+{
+    if (a == 0)
+        Console.WriteLine("Erreur dans la saisie");
+    else if (a < 3)
+        Console.WriteLine("Votre enfant est trop jeune");
+    else if (a >= 3 && a <= 6)
+        Console.WriteLine("Votre enfant est un baby");
+    else if (a >= 7 && a <= 8)
+        Console.WriteLine("Votre enfant est un poussin");
+    else if (a >= 9 && a <= 10)
+        Console.WriteLine("Votre enfant est un pupille");
+    else if (a >= 11 && a <= 12)
+        Console.WriteLine("Votre enfant est un minime");
+    else if (a >= 13 && a < 18)
+        Console.WriteLine("Votre enfant est un cadet");
+    else if (a >= 18)
+        Console.WriteLine("Ce n'est plus un enfant");
+}
+else
+{
+    Console.WriteLine("Veuillez entrer un âge valide.");
+}
