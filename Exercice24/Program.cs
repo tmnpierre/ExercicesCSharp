f5b60e390212870c201e191bfea89f291a86dc52
tmnpierre <tomyn.pierre@gmail.com>
2023-12-21 16:37:21 +0100

    feat: do exercice

--- Exercice24/Program.cs
+++ Exercice24/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Entrez un nombre : ");
+int nombre = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= nombre / 2; i++)
+{
+
+    for (int j = i; j <= nombre ; j++)
+    {
+        int somme = 0;
+        string suite = "";
+
+        for (int k = i; k <= j; k++)
+        {
+            somme += k;
+            suite += k.ToString();
+
+            if (k < j)
+            {
+                suite += " + ";
+            }
+        }
+
+        if (somme == nombre)
+        {
+            Console.WriteLine(suite + " = " + somme);
+        }
+
+    }
+}
