3dcca5982313b9ca5a8e6b3bd69c91560174a6e0
tmnpierre <tomyn.pierre@gmail.com>
2023-12-21 15:12:47 +0100

    feat: do exercice

---
+++ Exercice21/Program.cs
@@ -0,0 +1,8 @@
+﻿for (int chapter = 1; chapter <= 3; chapter++)
+{
+    Console.WriteLine($"Chapter {chapter}");
+    for (int subPart = 1; subPart <= 3; subPart++)
+    {
+        Console.WriteLine($"\tSubpart {chapter}.{subPart}");
+    }
+}
