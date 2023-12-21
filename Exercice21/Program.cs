9d08978643e23e71dc54396d8cc8e17561378679
tmnpierre <tomyn.pierre@gmail.com>
2023-12-21 15:14:09 +0100

    feat: do exercice

--- Exercice21/Program.cs
+++ Exercice21/Program.cs
@@ -1,4 +1,6 @@
-﻿for (int chapter = 1; chapter <= 3; chapter++)
+﻿Console.WriteLine("Tables of contents: ");
+
+for (int chapter = 1; chapter <= 3; chapter++)
 {
     Console.WriteLine($"Chapter {chapter}");
     for (int subPart = 1; subPart <= 3; subPart++)
