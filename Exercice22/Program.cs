302a667aaf2b6b84266ad3f28bbc3f341d9d6760
tmnpierre <tomyn.pierre@gmail.com>
2023-12-21 15:23:06 +0100

    feat: do exercice

---
+++ Exercice22/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Table de multiplication: ");
+Console.WriteLine();
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine($"Table de Multiplication de {i}");
+
+    for (int j = 1; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+
+    Console.WriteLine();
+}
