2e707a212286832c523eaf02404fc70d5514f366
tmnpierre <tomyn.pierre@gmail.com>
2023-12-21 15:58:12 +0100

    feat: do exercice

---
+++ Exercice23/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("--- Accroissement de la population ---");
+Console.WriteLine();
+
+double tauxDeCroissance = 0.89 / 100; 
+int populationActuelle = 96809;
+int populationCible = 120000;
+int anneeDepart = 2015;
+int annee;
+
+for (annee = anneeDepart; populationActuelle <= populationCible; annee++)
+{
+    populationActuelle += (int)(populationActuelle * tauxDeCroissance);
+}
+
+
+Console.WriteLine($"Il faudra {annee - anneeDepart} années pour dépasser {populationCible} habitants.");
+Console.WriteLine($"Il y aura {populationActuelle} habitants en l'année {annee}.");
