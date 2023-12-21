4ce801d4915543948468495ff55f670908fee424
tmnpierre <tomyn.pierre@gmail.com>
2023-12-21 11:44:00 +0100

    feat: do exercice

--- Exercice18/Program.cs
+++ Exercice18/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Indiquez l'âge de votre enfant: ");
+
+if (int.TryParse(Console.ReadLine(), out int age))
+{
+    switch (age)
+    {
+        case 0:
+            Console.WriteLine("Erreur dans la saisie");
+            break;
+        case < 3:
+            Console.WriteLine("Votre enfant est trop jeune");
+            break;
+        case <= 6:
+            Console.WriteLine("Votre enfant est un baby");
+            break;
+        case <= 8:
+            Console.WriteLine("Votre enfant est un poussin");
+            break;
+        case <= 10:
+            Console.WriteLine("Votre enfant est un pupille");
+            break;
+        case <= 12:
+            Console.WriteLine("Votre enfant est un minime");
+            break;
+        case < 18:
+            Console.WriteLine("Votre enfant est un cadet");
+            break;
+        default:
+            Console.WriteLine("Ce n'est plus un enfant");
+            break;
+    }
+}
+else
+{
+    Console.WriteLine("Veuillez entrer un âge valide.");
+}
