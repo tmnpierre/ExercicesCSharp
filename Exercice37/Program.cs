02b9a112da84e03c5ebdbe4c9b40300695ac47b4
tmnpierre <tomyn.pierre@gmail.com>
2024-01-02 21:58:25 +0100

    exercice

---
+++ Exercice37/Program.cs
@@ -0,0 +1,14 @@
+﻿string[] mois = { "Janvier", "Février", "Mars", "Avril", "Mai", "Juin",
+                              "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre" };
+
+Console.WriteLine("Énumération du tableau avec un foreach :");
+
+int index = 0;
+foreach (string moisName in mois)
+{
+    string tabs = new String('\t', index++);
+    Console.WriteLine(tabs + moisName);
+}
+
+Console.WriteLine("Appuyez sur Enter pour fermer le programme");
+Console.ReadKey();
