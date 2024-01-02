f8dec6fe02e1d8ae269caf7de72d358681f0c959
tmnpierre <tomyn.pierre@gmail.com>
2024-01-02 21:48:15 +0100

    exercice

---
+++ Exercice34/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Saisir la taille du tableau : ");
+var saisie = int.Parse(Console.ReadLine());
+
+var tab = new int[saisie];
+Random aleatoire = new Random();
+
+for (int i = 0; i < saisie; i++)
+{
+    tab[i] = aleatoire.Next(1, 51);
+}
+
+var tabulation = "\t";
+
+foreach (int i in tab)
+{
+    string paire;
+
+    if (i % 2 == 0)
+    {
+        paire = $"Le nombre {i} est pair";
+    }
+    else
+    {
+        paire = $"Le nombre {i} est impair";
+    }
+
+    Console.WriteLine($"{tabulation}{paire}");
+    tabulation += "\t";
+}
