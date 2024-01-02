5b4a8f70bcc176e5c18bee7ed28f5b92c0a303a3
tmnpierre <tomyn.pierre@gmail.com>
2024-01-02 10:30:09 +0100

    exercice

---
+++ Exercice28/Program.cs
@@ -0,0 +1,28 @@
+﻿Random aleatoire = new Random();
+int test = aleatoire.Next(1, 51);
+
+Console.Write("Entrez un nombre entre 1 et 50 : ");
+int reponse = int.Parse(Console.ReadLine());
+Console.WriteLine();
+int nbCoup = 1; 
+
+while (reponse != test)
+{
+    if (reponse > test)
+    {
+        Console.Write("Plus petit, entrez un nouveau nombre : ");
+        reponse = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+        nbCoup++;
+    }
+    else if (reponse < test)
+    {
+        Console.Write("Plus grand, entrez un nouveau nombre : ");
+        reponse = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+        nbCoup++;
+    }
+}
+
+Console.WriteLine("Bonne réponse");
+Console.WriteLine($"Vous avez trouver en {nbCoup} coups");
