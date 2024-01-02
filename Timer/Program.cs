76602b847daabf38fd2968577a0fc69f84fe7ec7
tmnpierre <tomyn.pierre@gmail.com>
2024-01-02 11:21:25 +0100

    exercice

---
+++ Timer/Program.cs
@@ -0,0 +1,13 @@
+﻿DateTime fin = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 11, 30, 0);
+var delta = fin - DateTime.Now;
+Console.ForegroundColor = ConsoleColor.DarkRed;
+while (delta.TotalSeconds > 0)
+{
+    Console.ForegroundColor = DateTime.Now.Millisecond - 500 >= 0 ? ConsoleColor.DarkRed : ConsoleColor.Red;
+    delta = fin - DateTime.Now;
+    Console.Write(delta.ToString(@"hh\:mm\:ss"));
+    Thread.Sleep(500);
+    Console.Clear();
+}
+Console.WriteLine("TEMPS ECOULÉ !");
+Console.ReadKey();
