ae75b8b6441f76dc831f689f4c440d75b53b8c72
tmnpierre <tomyn.pierre@gmail.com>
2024-01-02 15:51:12 +0100

    exercice

---
+++ Exercice32/Program.cs
@@ -0,0 +1,17 @@
+﻿var tab = new int[10];
+var valeur = 0;
+
+for (int i = 0; i < 10; i++)
+{
+    Console.Write($"Saisir la valeur {i + 1} : ");
+    valeur = int.Parse(Console.ReadLine());
+    tab[i] = valeur;
+}
+
+var tabulation = "\t"; 
+
+foreach (int i in tab)
+{
+    Console.WriteLine($"{tabulation}{i}");
+    tabulation += "\t";
+}
