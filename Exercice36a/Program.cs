f5584b9560d1118d75b229be87d7cc0e3ae65ee4
tmnpierre <tomyn.pierre@gmail.com>
2024-01-03 09:43:20 +0100

    exercice

--- Exercice36a/Program.cs
+++ Exercice36a/Program.cs
@@ -1,23 +1,46 @@
-﻿Console.Write("Entrez le nombre de notes /20 : ");
-int nombreDeNotes = int.Parse(Console.ReadLine());
+﻿Console.WriteLine("***** Tableaux des notes *****");
+Console.WriteLine();
+
+int nombreDeNotes;
+while (true)
+{
+    Console.Write("Combien de notes comportera le tableau : ");
+    if (int.TryParse(Console.ReadLine(), out nombreDeNotes) && nombreDeNotes > 0)
+    {
+        break;
+    }
+    Console.WriteLine("Erreur de saisie, merci de saisir un nombre entier positif.");
+}
+
 double[] notes = new double[nombreDeNotes];
 
 for (int i = 0; i < nombreDeNotes; i++)
 {
-    Console.Write($"Entrez la note {i + 1} /20 : ");
-    notes[i] = double.Parse(Console.ReadLine());
+    while (true)
+    {
+        Console.Write($"Merci de saisir la note {i + 1} : ");
+        if (double.TryParse(Console.ReadLine(), out double note) && note >= 0 && note <= 20)
+        {
+            notes[i] = note;
+            break;
+        }
+        Console.WriteLine("Erreur, merci de saisir un chiffre/nombre pour la note entre 0 et 20.");
+    }
 }
 
-Console.Write("\nToutes les notes : ");
-foreach (double note in notes)
+Console.WriteLine("\n--- Liste des notes ---");
+for (int i = 0; i < notes.Length; i++)
 {
-    Console.Write($"{note}/20");
+    Console.WriteLine($"La note {i + 1} est de : {notes[i]}/20");
 }
 
 double maxNote = notes.Max();
 double minNote = notes.Min();
 double moyenne = notes.Average();
 
-Console.WriteLine($"\nNote Max: {maxNote}");
-Console.WriteLine($"Note Min: {minNote}");
-Console.WriteLine($"Moyenne: {moyenne}");
+Console.WriteLine("\n--- La note max est : {0}/20", maxNote);
+Console.WriteLine("--- La note min est : {0}/20", minNote);
+Console.WriteLine("--- La moyenne est de : {0:0.00}/20", moyenne);
+
+Console.WriteLine("\nAppuyez sur Enter pour fermer le programme...");
+Console.ReadLine();
