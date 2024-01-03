31c7ca64d356290cb71b94c116ea126dc8251e8b
tmnpierre <tomyn.pierre@gmail.com>
2024-01-03 15:09:56 +0100

    exercice

---
+++ Exercice40Fonctions/Program.cs
@@ -0,0 +1,12 @@
+﻿string NomComplet(string prenom, string nom)
+{ 
+    return $"{prenom} + ' ' + {nom}";
+}
+
+Console.WriteLine(NomComplet("Joe", "Doe"));
+void soustraction(int a, int b)
+{
+    int sum = a - b;
+    Console.WriteLine($"Je soustrait {a} et {b}")
+}
+
