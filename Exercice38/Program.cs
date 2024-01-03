4020366510f6b244f6186a428b8e444adc0a4f23
tmnpierre <tomyn.pierre@gmail.com>
2024-01-03 11:10:00 +0100

    exercice

--- Exercice38/Program.cs
+++ Exercice38/Program.cs
@@ -1,10 +1,35 @@
-﻿var tab = new int[10];
-int valeur;
-Random aleatoire = new Random();
+﻿Console.WriteLine("--- Où est passé mon numéro ? ---");
+Console.WriteLine("Affectation des valeurs...");
 
-for (int i = 0; i < 10; i++)
+Random random = new Random();
+int[] numbers = new int[10];
+var tabulation = "\t";
+var tabulation2 = "\t";
+
+for (int i = 0; i < numbers.Length; i++)
 {
-    Console.Write($"Saisir la valeur {i + 1} : ");
-    valeur = aleatoire.Next(1, 51);
-    tab[i] = valeur;
+    numbers[i] = random.Next(1, 51);
 }
+
+Console.WriteLine("\nAffichage avant triage :");
+foreach (int number in numbers)
+{
+    Console.WriteLine($"{tabulation}{number}");
+    tabulation += "\t";
+}
+
+int firstNumber = numbers[0];
+
+Array.Sort(numbers);
+
+Console.WriteLine("\nAprès :");
+foreach (int number in numbers)
+{
+    Console.WriteLine($"{tabulation2}{number}");
+    tabulation2 += "\t";
+}
+
+int newPosition = Array.IndexOf(numbers, firstNumber) + 1;
+
+Console.WriteLine($"\nLe nombre {firstNumber} se trouvait en 1ere position");
+Console.WriteLine($"Il se retrouve à la position {newPosition} après triage.");
