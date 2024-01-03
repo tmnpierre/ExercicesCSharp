09a569d21ecbf6877d57a364f79198d0db1c675b
tmnpierre <tomyn.pierre@gmail.com>
2024-01-03 10:13:16 +0100

    exercice

--- Exercice38/Program.cs
+++ Exercice38/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var tab = new int[10];
+int valeur;
+Random aleatoire = new Random();
+
+for (int i = 0; i < 10; i++)
+{
+    Console.Write($"Saisir la valeur {i + 1} : ");
+    valeur = aleatoire.Next(1, 51);
+    tab[i] = valeur;
+}
