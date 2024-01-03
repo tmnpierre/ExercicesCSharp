fd44c8233fb8b7413211d9e576315a9c9f6c9673
tmnpierre <tomyn.pierre@gmail.com>
2024-01-03 16:01:07 +0100

    exercice

--- Exercice40Fonctions/Program.cs
+++ Exercice40Fonctions/Program.cs
@@ -1,12 +1,40 @@
 ﻿string NomComplet(string prenom, string nom)
 { 
-    return $"{prenom} + ' ' + {nom}";
+    return prenom + " " + nom;
 }
 
 Console.WriteLine(NomComplet("Joe", "Doe"));
-void soustraction(int a, int b)
+
+int Soustraire(int a,  int b)
 {
-    int sum = a - b;
-    Console.WriteLine($"Je soustrait {a} et {b}")
+    Console.WriteLine($"Je soustrait {a} et {b}");
+    return a - b ;
 }
 
+int sub = Soustraire(2, 1);
+Console.WriteLine("Résulat : " + sub);
+
+static string QuelleHeure(string heure = "12h00")
+{
+    return heure;
+}
+
+Console.WriteLine(QuelleHeure());
+Console.WriteLine(QuelleHeure("14h00"));
+
+static int CompteurLettreA(string chaine)
+{
+    int compteur = 0;
+    foreach (char lettre in chaine)
+    {
+        if (lettre == 'a' || lettre == 'A')
+        {
+            compteur++;
+        }
+    }
+    Console.Write("Resultat : ");
+    return compteur;
+}
+
+Console.WriteLine(CompteurLettreA("Banana"));
+Console.WriteLine(CompteurLettreA("Bonjour"));
