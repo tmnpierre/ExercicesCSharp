f1a8566e3d20367e1cc70abb0270d2dc7ba7d561
tmnpierre <tomyn.pierre@gmail.com>
2024-01-03 09:23:16 +0100

    exercice

---
+++ Exercice33/Program.cs
@@ -0,0 +1,14 @@
+﻿List<char> alphabet = new List<char>();
+
+for (int i = 'A'; i <= 'Z'; i++)
+    alphabet.Add((char)i);
+
+Console.WriteLine("Affichage des valeurs du alphabetleau :");
+
+string espaces = "";
+
+for (int i = 0; i < alphabet.Count; i++)
+{
+    Console.WriteLine(espaces + alphabet[i]);
+    espaces += "  ";
+}
